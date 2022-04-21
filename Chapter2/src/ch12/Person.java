34183ba325809296d545f3ac7e05aa7df7d8ab02
JAE <jack3787@naver.com>
2022-04-21 20:27:06 +0900

    this

--- Chapter2/src/ch12/Person.java
+++ Chapter2/src/ch12/Person.java
@@ -2,4 +2,34 @@ package ch12;
 
 public class Person {
 
+    String name;
+    int age;
+
+    public Person(){ // default 생성자
+        this("no name", 1);
+    }
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void showPerson() {
+        System.out.println(name + "," + age);
+
+    }
+
+    public Person getPerson() {
+        return this;
+    }
+
+    public static void main(String[] args){
+        Person person = new Person(); //default 생성자
+        person.showPerson();
+
+        System.out.println(person);
+
+        Person person2 = person.getPerson();
+        System.out.println();
+    }
 }
